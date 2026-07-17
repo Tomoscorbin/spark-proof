5686bfe58372351d87918b61523e63a543265b10
Tomos Corbin <tom_corbin1@hotmail.com>
2026-07-17 15:32:35 +0100

    chore: remove incomplete RangeField migration, drop faker

--- examples/test_integer.py
+++
@@ -1,9 +0,0 @@
-from datetime import date
-from spark_proof.api import date_field
-
-
-gen = date_field()
-strategy = gen.strategy
-
-samples = [strategy.example() for _ in range(20)]
-print(samples)
